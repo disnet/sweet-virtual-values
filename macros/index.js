51cb5d9cbd80ba31937e64c3d8e66bf2c3510842
Tim Disney <tim.disney@gmail.com>
2014-06-27 17:05:35 -0700

    adding in all the custom operators

--- macros/index.js
+++ macros/index.js
@@ -28,8 +28,74 @@ let unproxy = macro {
     }
 }
 
-operator - 14 { $op } => #{ vvalues.unary("-", $op) }
-export -;
-
-operator + 12 left { $left, $right } => #{ vvalues.binary("+", $left, $right) }
+operator new 16 { $op }    => #{ vvalues.isVProxy($op) ? vvalues.unary("new", $op) : new $op }
+operator ++ 15 { $op }     => #{ vvalues.unary("++", $op) }
+operator -- 15 { $op }     => #{ vvalues.unary("--", $op) }
+operator ! 14 { $op }      => #{ vvalues.unary("!", $op) }
+operator ~ 14 { $op }      => #{ vvalues.unary("~", $op) }
+operator + 14 { $op }      => #{ vvalues.unary("+", $op) }
+operator - 14 { $op }      => #{ vvalues.unary("-", $op) }
+operator typeof 14 { $op } => #{ vvalues.unary("typeof", $op) }
+operator void 14 { $op }   => #{ vvalues.unary("void", $op) }
+operator delete 14 { $op } => #{ vvalues.unary("delete", $op) }
+// export new;
+export ++;
+export --;
+export !;
+export ~;
 export +;
+export -;
+export typeof;
+export void;
+export delete;
+
+operator * 13 left { $left, $right }          => #{ vvalues.binary("*", $left, $right) }
+operator / 13 left { $left, $right }          => #{ vvalues.binary("/", $left, $right) }
+operator % 13 left { $left, $right }          => #{ vvalues.binary("%", $left, $right) }
+operator + 12 left { $left, $right }          => #{ vvalues.binary("+", $left, $right) }
+operator - 12 left { $left, $right }          => #{ vvalues.binary("-", $left, $right) }
+operator >> 11 left { $left, $right }         => #{ vvalues.binary(">>", $left, $right) }
+operator << 11 left { $left, $right }         => #{ vvalues.binary("<<", $left, $right) }
+operator >>> 11 left { $left, $right }        => #{ vvalues.binary(">>>", $left, $right) }
+operator < 10 left { $left, $right }          => #{ vvalues.binary("<", $left, $right) }
+operator <= 10 left { $left, $right }         => #{ vvalues.binary("<=", $left, $right) }
+operator >= 10 left { $left, $right }         => #{ vvalues.binary(">", $left, $right) }
+operator > 10 left { $left, $right }          => #{ vvalues.binary(">=", $left, $right) }
+operator in 10 left { $left, $right }         => #{ vvalues.binary("in", $left, $right) }
+operator instanceof 10 left { $left, $right } => #{ vvalues.binary("instanceof", $left, $right) }
+operator == 9 left { $left, $right }          => #{ vvalues.binary("==", $left, $right) }
+operator != 9 left { $left, $right }          => #{ vvalues.binary("!=", $left, $right) }
+operator === 9 left { $left, $right }         => #{ vvalues.binary("===", $left, $right) }
+operator !== 9 left { $left, $right }         => #{ vvalues.binary("!==", $left, $right) }
+operator & 8 left { $left, $right }           => #{ vvalues.binary("&", $left, $right) }
+operator ^ 7 left { $left, $right }           => #{ vvalues.binary("^", $left, $right) }
+operator | 6 left { $left, $right }           => #{ vvalues.binary("|", $left, $right) }
+operator && 5 left { $left, $right }          => #{ vvalues.binary("&&", $left, $right) }
+operator || 4 left { $left, $right }          => #{ vvalues.binary("||", $left, $right) }
+export *;
+export /;
+export %;
+export +;
+export -;
+export >>;
+export <<;
+export >>>;
+export <;
+export <=;
+export >;
+export >=;
+export in;
+export instanceof;
+export ==;
+export !=;
+export !==;
+export ===;
+export &;
+export ^;
+export |;
+export &&;
+export ||;
+
+
+
+
