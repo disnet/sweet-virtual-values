b94df34c9cd91206146b14bf56cc19f5a49d0a10
Tim Disney <tim.disney@gmail.com>
2014-07-04 00:11:02 -0700

    emberifying everything

--- js/app.js
+++ js/app.js
@@ -11,53 +11,97 @@ function cleanerEval(str, oldConsole) {
 	return logArr;
 }
 
-$(document).ready(function() {
-    var vvalues = returnExports;
-	var editor, compiled;
 
-	function updateExamples() {
-		var exampleText = $(this).find("pre").text();
-		editor.setValue(exampleText)
-	}
+var examples = [
+    {
+        id: 1,
+        file: "complex.js",
+        title: "Complex Numbers"
+    },
+    {
+        id: 2,
+        file: "taint.js",
+        title: "Taiting"
+    }
+];
+
+var editor, compiled;
+var vvalues = returnExports;
+
+App = Ember.Application.create({});
+
+App.Router.map(function() {
+    this.route("about");
+    this.resource("examples", { path: "/"}, function() {
+        this.resource("example", {path: ":example_id"});
+    });
+});
+
+
+App.ExamplesController = Ember.ObjectController.extend({
+    errors: "",
+    logs: [],
+    run: false,
+    actions: {
+        select: function(example) {
+            Ember.$.ajax("examples/" + example.file, {
+                dataType: "text"
+            }).then(function(code) {
+                editor.setValue(code);
+            });
+        },
+        run: function() {
+            this.set("errors", "");
+		    try {
+			    var out = vvalues.compile(editor.getValue(), {
+				    readableNames: true
+			    });
+			    compiled.setValue(out);
+			    var logArr = cleanerEval(out, console);
+                this.set("run", true);
+                this.set("logs", logArr.map(function(l) {
+                    return { l: l };
+                }));
+
+		    } catch (e) {
+                this.set("errors", e);
+		    }
+        }
+    }
+});
+
+App.ExamplesRoute = Ember.Route.extend({
+    model: function() {
+        return examples;
+    }
+
+});
+
+App.ExamplesView = Ember.View.extend({
+    didInsertElement: function() {
+        editor = CodeMirror.fromTextArea($("#editor")[0], {
+	        mode: "javascript",
+	        lineNumbers: true
+        });
+
+        compiled = CodeMirror.fromTextArea($("#compiled")[0], {
+	        mode: "javascript",
+	        lineNumbers: true,
+	        readOnly: true
+        });
+    }
+})
+App.ExampleRoute = Ember.Route.extend({
+    model: function(params) {
+        return examples.findBy("id", params.example_id);
+    }
+})
+
+
+$(document).ready(function() {
 
 	function updateCompiled() {
-		$("#error-box").hide();
-		$("#run-box").hide();
-		try {
-			var out = vvalues.compile(editor.getValue(), {
-				readableNames: true
-			});
-			compiled.setValue(out);
-			var logArr = cleanerEval(out, console);
-			var logHtml = logArr.map(function(l) {
-				return "<p>Log: " + l + "</p>";
-			})
-			$("#run-box").show().html("<h4 class='success-label'>Success</h4>"+ 
-									  logHtml.join("\n"));
-
-		} catch (e) {
-			$("#error-box").show();
-			$("#error-box").html("<pre>" + e + "</pre>");
-		}
 	}
 
-	function init() {
-		editor = CodeMirror.fromTextArea($("#editor")[0], {
-			mode: "javascript",
-			lineNumbers: true
-		});
 
-		compiled = CodeMirror.fromTextArea($("#compiled")[0], {
-			mode: "javascript",
-			lineNumbers: true,
-			readOnly: true
-		});
-
-		$("#btn-examples li").click(updateExamples);
-		$("#btn-run").click(updateCompiled)
-
-		$("#btn-examples li:first").trigger("click");
-	}
-
-    init();
 });
