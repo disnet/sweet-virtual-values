0ca04c628b5543d809007e9c2621db53396b455e
Tim Disney <tim.disney@gmail.com>
2014-07-06 17:21:38 -0700

    adding complex number example

--- examples/complex.js
+++ examples/complex.js
@@ -1,1 +1,68 @@
-// complex numbers
+var key = {};
+
+var unaryOps = {
+    "-": function(real, img) {
+        return makeComplex(-real, -img);
+    }
+    // ...
+};
+
+var binaryOps = {
+    "+": function(lReal, lImg, rReal, rImg) {
+        return makeComplex(lReal + rReal, lImg + rImg);
+    },
+    "-": function(lReal, lImg, rReal, rImg) {
+        return makeComplex(lReal - rReal, lImg - rImg);
+    },
+    "*": function(lReal, lImg, rReal, rImg) {
+        return makeComplex((lReal * rReal) - (lImg * rImg),
+                           (lImg * rReal) + (lReal * rImg));
+    },
+    "/": function(lReal, lImg, rReal, rImg) {
+        return makeComplex(((lReal * rReal) + (lImg * rImg)) / ((rReal * rReal) + (rImg * rImg)),
+                           ((lImg * rReal - lReal * rImg) / ((rReal * rReal) + (rImg * rImg))));
+    },
+    "===": function(lReal, lImg, rReal, rImg) {
+        return (lReal === rReal) && (lImg === rImg);
+    }
+    // ...
+};
+
+function C() {}
+C.prototype.toString = function() {
+    var handler = unproxy(this, key);
+    return handler.real + " + " + handler.img + "i";
+};
+
+function makeComplex(real, img) {
+    var p = new Proxy(new C(), {
+        real: real,
+        img: img,
+        unary: function(target, op) {
+            return unaryOps[op](real, img);
+        },
+
+        left: function(target, op, right) {
+            var rhandler = unproxy(right, key);
+            if (rhandler) {
+                return binaryOps[op](real, img, rhandler.real, rhandler.img);
+            } else {
+                return binaryOps[op](real, img, right, 0);
+            }
+        },
+        right: function(target, op, left) {
+            return binaryOps[op](left, 0, real, img);
+        }
+    });
+}
+
+function isComplex(value) {
+    if(unproxy(value, key)) {
+        return true;
+    }
+    return false;
+}
+
+var i = makeComplex(0, 1);
+
+console.log(1 + (1 * i));
